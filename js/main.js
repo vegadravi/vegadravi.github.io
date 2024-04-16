5444badad4d0d839cfebc1eabd571915b64d5e4b
Vegad Ravi <98304481+vegadravi@users.noreply.github.com>
2024-04-16 20:28:45 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,97 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    function filterJobs(keyword) {
+        var jobProfiles = document.querySelectorAll('.job-profile');
+
+        jobProfiles.forEach(function(profile) {
+            
+            if (profile.innerText.toLowerCase().includes(keyword.toLowerCase())) {
+                
+                profile.style.display = 'block';
+            } else {
+                profile.style.display = 'none';
+            }
+        });
+    }
+    document.getElementById('searchBtn').addEventListener('click', function() {
+        var keyword = document.getElementById('keywordInput').value;
+        filterJobs(keyword);
+    });
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').css('top', '0px');
+        } else {
+            $('.sticky-top').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+})(jQuery);
+
